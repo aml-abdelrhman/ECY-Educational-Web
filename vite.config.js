78a8042d063c8ae264044ee08fabe42b9f403ed4
aml-abdelrhman <Aml586651@gmail.com>
2025-09-06 20:12:25 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/ECY-Educational-Web/',
+  base: '/vite-project/', // أو اسم المستودع على GitHub
   plugins: [react()],
-})
+});
