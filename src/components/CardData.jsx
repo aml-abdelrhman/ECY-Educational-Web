c5fb326252b7f778edbbb5c4258ba78693c59516
aml-abdelrhman <Aml586651@gmail.com>
2025-09-06 20:06:28 +0300

    update

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,24 +1,29 @@
+import a from './assets/images/a.jpg';
+import b from './assets/images/b.jpg';
+import c from './assets/images/c.jpg';
+
 const CardData = [
   {
     id: 1,
-    img: "/images/a.jpg",
+    img: a,
     title: "Box Library",
     description: "A complete library of educational resources, articles, and e-books to help you improve your skills quickly and easily.",
     color: "#e3f2fd"
   },
   {
     id: 2,
-    img: "/images/b.jpg",
+    img: b,
     title: "Market Analysis",
     description: "Accurate tools and reports to analyze markets, understand trends, and make better business and investment decisions.",
     color: "#f1f8e9"
   },
   {
     id: 3,
-    img: "/images/c.jpg",
+    img: c,
     title: "Online Courses",
     description: "A collection of online training courses covering various fields such as technology, languages, and management to expand your knowledge.",
     color: "#fff3e0"
   }
 ];
+
 export default CardData;
